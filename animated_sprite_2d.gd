f802679b02cc61de8a901eade57f233d2484da77
abhaytippi <abhay.tippi03@gmail.com>
2025-09-27 15:30:39 -0500

    run back slow

--- animated_sprite_2d.gd
+++ animated_sprite_2d.gd
@@ -3,5 +3,7 @@ extends AnimatedSprite2D
 func _process(_delta):
 	if Input.is_action_pressed("ui_right"):
 		play("run")
+	elif Input.is_action_pressed("ui_left"):
+		play("run_back")
 	else:
 		stop()
