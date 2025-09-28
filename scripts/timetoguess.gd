a695daf4c5ab203979364bf247ebb98314bbce77
abhaytippi <abhay.tippi03@gmail.com>
2025-09-28 09:20:19 -0500

    making end game

---
+++ scripts/timetoguess.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+
+func _on_wrong_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/MainGame.tscn")
+
+
+func _on_correct_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/MainGame.tscn")
+
+
+func _on_wrong_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/MainGame.tscn")
