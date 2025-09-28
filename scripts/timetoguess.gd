53f61e30b4d1453dea42444b99dba09f809f0e70
abhaytippi <abhay.tippi03@gmail.com>
2025-09-28 09:39:22 -0500

    added win and lose screen

--- scripts/timetoguess.gd
+++ scripts/timetoguess.gd
@@ -3,12 +3,12 @@ extends Node2D
 
 
 func _on_wrong_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/MainGame.tscn")
+	get_tree().change_scene_to_file("res://scenes/dead.tscn")
 
 
 func _on_correct_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/MainGame.tscn")
+	get_tree().change_scene_to_file("res://scenes/winner.tscn")
 
 
 func _on_wrong_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/MainGame.tscn")
+	get_tree().change_scene_to_file("res://scenes/dead.tscn")
