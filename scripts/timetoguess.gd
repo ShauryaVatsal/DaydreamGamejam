8525a22c1386816161125858b94ee109d7f9eb29
ShauryaVatsal <vatshaurya@gmail.com>
2025-09-28 11:09:47 -0500

    win screen

--- scripts/timetoguess.gd
+++ scripts/timetoguess.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
-func _input(event):
-	if event is InputEventKey and event.pressed and not event.echo:
-		if event.keycode == KEY_ESCAPE:
-			get_tree().change_scene_to_file("res://scenes/MainGame.tscn")
+@onready var correct = $correct
+@onready var wrong = $wrong
+@onready var wrong2 = $wrong2
+
+func _ready():
+	correct.presed.connect(_on_correct_pressed)
+	wrong.presed.connect(_on_wrong_pressed)
+	wrong1.presed.connect(_on_wrong_pressed)
+
 
 func _input2(event):
 	if event is InputEventKey and event.pressed and not event.echo:
