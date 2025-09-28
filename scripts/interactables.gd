bb8b77501b025f1c611915049c517519133874cc
abhaytippi <abhay.tippi03@gmail.com>
2025-09-28 03:08:04 -0500

    added back to main

--- scripts/interactables.gd
+++ scripts/interactables.gd
@@ -4,4 +4,7 @@ extends Node
 @onready var letter = $Letter
 
 func _ready():
-		envelope.pressed.connect(letter._on_button_pressed) 
+	envelope.pressed.connect(letter._on_button_pressed)
+
+func _on_backtohall_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/MainGame.tscn")
