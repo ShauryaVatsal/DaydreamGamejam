cb88e05c90841a77767d82fb97514a18445c3aab
abhaytippi <abhay.tippi03@gmail.com>
2025-09-28 11:45:12 -0500

    grfhjsdkasj

--- scenes/sprite_2d.gd
+++ scenes/sprite_2d.gd
@@ -4,5 +4,3 @@ func _input(event):
 	if event is InputEventKey and event.pressed and not event.echo:
 		if event.keycode == KEY_ESCAPE:
 			get_tree().change_scene_to_file("res://scenes/MainGame.tscn")
-	
-	
