706056363defae78bb496cf5ed31746103aa8bbf
abhaytippi <abhay.tippi03@gmail.com>
2025-09-28 11:22:07 -0500

    dshfiushdfdh

---
+++ scenes/sprite_2d.gd
@@ -0,0 +1,8 @@
+extends Sprite2D
+
+func _input(event):
+	if event is InputEventKey and event.pressed and not event.echo:
+		if event.keycode == KEY_ESCAPE:
+			get_tree().change_scene_to_file("res://scenes/MainGame.tscn")
+	
+	
