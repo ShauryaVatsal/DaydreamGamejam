6bdbaa4835489ee395b5df9c7b3e3b14f595b684
abhaytippi <abhay.tippi03@gmail.com>
2025-09-27 22:03:16 -0500

    outline and animation for kid room

--- scenes/texture_button.gd
+++ scenes/texture_button.gd
@@ -1,11 +1,24 @@
 extends TextureButton
 
-@onready var label = $"../Label"
+@onready var paper = $"../Paper"
+var open = false
 
 func _ready():
 	pressed.connect(_button_pressed)
 	
 
 func _button_pressed():
-	label.visible = true
-	$"../Paper".visible = true
+	open = not open
+	if open:
+		open_letter()
+	else:
+		close_letter()
+	
+
+func open_letter():
+	var tween = get_tree().create_tween()
+	tween.tween_property(paper, "position", Vector2(574, 309), 1).set_trans(Tween.TRANS_SINE)
+	
+func close_letter():
+	var tween = get_tree().create_tween()
+	tween.tween_property(paper, "position", Vector2(574, 1005), 1).set_trans(Tween.TRANS_SINE)
