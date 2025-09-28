8c257c89d21fd5bced0154c395f5ff55bfabd56f
ShauryaVatsal <vatshaurya@gmail.com>
2025-09-28 03:00:10 -0500

    added start screen and buttons

---
+++ scenes/play.gd
@@ -0,0 +1,7 @@
+extends Button
+
+func _ready() -> void:
+	pressed.connect(play)
+
+func play():
+	get_tree().change_scene_to_file("res://scenes/hell.tscn")
--- scripts/Globals.gd
+++ scripts/Globals.gd
@@ -4,6 +4,10 @@ var sanity = 0
 var inHell = true
 signal sanity_changed(amount)
 
+func _ready() -> void:
+	set_sanity(100)
+	
+
 func set_sanity(amount):
 	sanity = amount
 	sanity_changed.emit(sanity)
@@ -13,3 +17,9 @@ func get_sanity():
 
 func turnplace(boo):
 	inHell = boo
+
+func _process(delta: float) -> void:
+	if not inHell:
+		set_sanity(get_sanity() - delta)
+		if get_sanity() <= 0:
+			print("Game Lost")
